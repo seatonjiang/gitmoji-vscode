ede6bfbea90e8f0c6d3f732a52b90a4d40471306
Seaton Jiang <seaton@vtrois.com>
2020-05-23 21:48:39 +0800

    style: change code indentation

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,13 @@ import * as vscode from 'vscode';
 import { GitExtension, Repository } from './api/git';
 import Gitmoji from './gitmoji/gitmoji';
 
-export function activate(context: vscode.ExtensionContext){
+export function activate(context: vscode.ExtensionContext) {
 
     let disposable = vscode.commands.registerCommand('extension.Gitmoji', (uri?) => {
         const git = getGitExtension();
         const language = getEnvLanguage();
 
-        if (!git){
+        if (!git) {
             vscode.window.showErrorMessage('Unable to load Git Extension');
             return;
         }
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext){
         let emojis = [];
         let onlyUseAdditionalEmojis: boolean | undefined = vscode.workspace.getConfiguration().get("gitmoji.onlyUseAdditionalEmojis");
 
-        if(onlyUseAdditionalEmojis === true){
+        if(onlyUseAdditionalEmojis === true) {
             emojis = [...additionalEmojis];
         } else {
             emojis = [...Gitmoji, ...additionalEmojis];
@@ -25,8 +25,8 @@ export function activate(context: vscode.ExtensionContext){
 
         let items = [];
 
-        if (language === "zh-cn"){
-            for (let i = 0; i < emojis.length; i++){
+        if (language === "zh-cn") {
+            for (let i = 0; i < emojis.length; i++) {
                 items.push({
                 label: `${emojis[i].emoji} ${emojis[i].description_zh_cn || emojis[i].description}`,
                 code: emojis[i].code,
@@ -34,7 +34,7 @@ export function activate(context: vscode.ExtensionContext){
                 });
             }
         } else {
-            for (let i = 0; i < emojis.length; i++){
+            for (let i = 0; i < emojis.length; i++) {
                 items.push({
                 label: `${emojis[i].emoji} ${emojis[i].description}`,
                 code: emojis[i].code,
@@ -43,25 +43,25 @@ export function activate(context: vscode.ExtensionContext){
             }
         }
 
-        vscode.window.showQuickPick(items).then(function (selected){
-            if (selected){
+        vscode.window.showQuickPick(items).then(function (selected) {
+            if (selected) {
                 vscode.commands.executeCommand('workbench.view.scm');
                 let outputType = vscode.workspace.getConfiguration().get('gitmoji.outputType');
 
-                if (uri){
+                if (uri) {
                     let selectedRepository = git.repositories.find(repository => {
                         return repository.rootUri.path === uri._rootUri.path;
                     });
-                    if (selectedRepository){
-                        if (outputType === "emoji"){
+                    if (selectedRepository) {
+                        if (outputType === "emoji") {
                             prefixCommit(selectedRepository, selected.emoji);
                         } else {
                             prefixCommit(selectedRepository, selected.code);
                         }
                     }
                 } else {
-                    for (let repo of git.repositories){
-                        if (outputType === "emoji"){
+                    for (let repo of git.repositories) {
+                        if (outputType === "emoji") {
                             prefixCommit(repo, selected.emoji);
                         } else {
                             prefixCommit(repo, selected.code);
@@ -75,19 +75,19 @@ export function activate(context: vscode.ExtensionContext){
     context.subscriptions.push(disposable);
 }
 
-function getEnvLanguage(){
+function getEnvLanguage() {
     const language = vscode.env.language;
     return language;
 }
 
-function prefixCommit(repository: Repository, prefix: String){
+function prefixCommit(repository: Repository, prefix: String) {
     repository.inputBox.value = `${prefix} ${repository.inputBox.value}`;
 }
 
-function getGitExtension(){
+function getGitExtension() {
     const vscodeGit = vscode.extensions.getExtension<GitExtension>('vscode.git');
     const gitExtension = vscodeGit && vscodeGit.exports;
     return gitExtension && gitExtension.getAPI(1);
 }
 
-export function deactivate(){}
+export function deactivate() {}
