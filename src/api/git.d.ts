c8480a96b52eb2ac594a407cc1087261d3e4f86c
Seaton Jiang <seaton@vtrois.com>
2020-10-04 12:47:28 +0800

    feat: update VSCode api

--- src/api/git.d.ts
+++ src/api/git.d.ts
@@ -4,7 +4,8 @@
  *  Links: https://github.com/microsoft/vscode/blob/master/extensions/git/src/api/git.d.ts
  *--------------------------------------------------------------------------------------------*/
 
-import { Uri, SourceControlInputBox, Event, CancellationToken } from "vscode";
+import { Uri, Event, Disposable, ProviderResult } from "vscode";
+export { ProviderResult } from "vscode";
 
 export interface Git {
   readonly path: string;
@@ -42,7 +43,10 @@ export interface Commit {
   readonly hash: string;
   readonly message: string;
   readonly parents: string[];
-  readonly authorEmail?: string | undefined;
+  readonly authorDate?: Date;
+  readonly authorName?: string;
+  readonly authorEmail?: string;
+  readonly commitDate?: Date;
 }
 
 export interface Submodule {
@@ -117,6 +121,23 @@ export interface RepositoryUIState {
 export interface LogOptions {
   /** Max number of log entries to retrieve. If not specified, the default is 32. */
   readonly maxEntries?: number;
+  readonly path?: string;
+}
+
+export interface CommitOptions {
+  all?: boolean | "tracked";
+  amend?: boolean;
+  signoff?: boolean;
+  signCommit?: boolean;
+  empty?: boolean;
+  noVerify?: boolean;
+}
+
+export interface BranchQuery {
+  readonly remote?: boolean;
+  readonly pattern?: string;
+  readonly count?: number;
+  readonly contains?: string;
 }
 
 export interface Repository {
@@ -162,6 +183,7 @@ export interface Repository {
   createBranch(name: string, checkout: boolean, ref?: string): Promise<void>;
   deleteBranch(name: string, force?: boolean): Promise<void>;
   getBranch(name: string): Promise<Branch>;
+  getBranches(query: BranchQuery): Promise<Ref[]>;
   setBranchUpstream(name: string, upstream: string): Promise<void>;
 
   getMergeBase(ref1: string, ref2: string): Promise<string>;
@@ -171,6 +193,7 @@ export interface Repository {
 
   addRemote(name: string, url: string): Promise<void>;
   removeRemote(name: string): Promise<void>;
+  renameRemote(name: string, newName: string): Promise<void>;
 
   fetch(remote?: string, ref?: string, depth?: number): Promise<void>;
   pull(unshallow?: boolean): Promise<void>;
@@ -182,6 +205,40 @@ export interface Repository {
 
   blame(path: string): Promise<string>;
   log(options?: LogOptions): Promise<Commit[]>;
+
+  commit(message: string, opts?: CommitOptions): Promise<void>;
+}
+
+export interface RemoteSource {
+  readonly name: string;
+  readonly description?: string;
+  readonly url: string | string[];
+}
+
+export interface RemoteSourceProvider {
+  readonly name: string;
+  readonly icon?: string; // codicon name
+  readonly supportsQuery?: boolean;
+  getRemoteSources(query?: string): ProviderResult<RemoteSource[]>;
+  publishRepository?(repository: Repository): Promise<void>;
+}
+
+export interface Credentials {
+  readonly username: string;
+  readonly password: string;
+}
+
+export interface CredentialsProvider {
+  getCredentials(host: Uri): ProviderResult<Credentials>;
+}
+
+export interface PushErrorHandler {
+  handlePushError(
+    repository: Repository,
+    remote: Remote,
+    refspec: string,
+    error: Error & { gitErrorCode: GitErrorCodes }
+  ): Promise<boolean>;
 }
 
 export type APIState = "uninitialized" | "initialized";
@@ -193,6 +250,14 @@ export interface API {
   readonly repositories: Repository[];
   readonly onDidOpenRepository: Event<Repository>;
   readonly onDidCloseRepository: Event<Repository>;
+
+  toGitUri(uri: Uri, ref: string): Uri;
+  getRepository(uri: Uri): Repository | null;
+  init(root: Uri): Promise<Repository | null>;
+
+  registerRemoteSourceProvider(provider: RemoteSourceProvider): Disposable;
+  registerCredentialsProvider(provider: CredentialsProvider): Disposable;
+  registerPushErrorHandler(handler: PushErrorHandler): Disposable;
 }
 
 export interface GitExtension {
@@ -229,6 +294,7 @@ export const enum GitErrorCodes {
   CantOpenResource = "CantOpenResource",
   GitNotFound = "GitNotFound",
   CantCreatePipe = "CantCreatePipe",
+  PermissionDenied = "PermissionDenied",
   CantAccessRemote = "CantAccessRemote",
   RepositoryNotFound = "RepositoryNotFound",
   RepositoryIsLocked = "RepositoryIsLocked",
