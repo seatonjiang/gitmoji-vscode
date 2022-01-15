6ccfb25489b2684ba1cda01187f51f2ff5e020ee
Seaton Jiang <hi@seatonjiang.com>
2022-01-15 17:52:07 +0800

    chore: add project url

--- src/api/git.d.ts
+++ src/api/git.d.ts
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  Links: https://github.com/microsoft/vscode/blob/master/extensions/git/src/api/git.d.ts
  *--------------------------------------------------------------------------------------------*/
 
 import { Uri, Event, Disposable, ProviderResult } from 'vscode';
